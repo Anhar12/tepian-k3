e5b7d2dbcc607b80302dcb36e348e8aea3d1e91d
rizrmdhn <rizrmdhn.work@gmail.com>
2025-12-16 14:58:13 +0800

    Refactor code structure for improved readability and maintainability

---
+++ packages/db/src/migrations/0000_icy_daredevil.sql
@@ -0,0 +1,11 @@
+CREATE TABLE "users" (
+	"id" uuid PRIMARY KEY NOT NULL,
+	"username" varchar(50) NOT NULL,
+	"password" varchar(150) NOT NULL,
+	"created_at" timestamp with time zone NOT NULL,
+	"updated_at" timestamp with time zone,
+	CONSTRAINT "username_unique" UNIQUE("username")
+);
+--> statement-breakpoint
+CREATE INDEX "username_idx" ON "users" USING btree ("username");--> statement-breakpoint
+CREATE INDEX "user_idx" ON "users" USING btree ("id");
