65d5a17c38e15caa548514c9cb29d9476c97c866
rizrmdhn <114452320+rizrmdhn@users.noreply.github.com>
2026-01-09 07:06:14 +0800

    Add migration entry for version 7 with tag '0003_foamy_gambit'

---
+++ packages/db/src/migrations/0003_foamy_gambit.sql
@@ -0,0 +1,7 @@
+ALTER TABLE "orders" DROP COLUMN "offering_document_file_name";--> statement-breakpoint
+ALTER TABLE "orders" DROP COLUMN "offering_user_document_file_name";--> statement-breakpoint
+ALTER TABLE "orders" DROP COLUMN "invoice_file_name";--> statement-breakpoint
+ALTER TABLE "orders" DROP COLUMN "proof_of_payment_file_name";--> statement-breakpoint
+ALTER TABLE "orders" DROP COLUMN "assignment_letter_file_name";--> statement-breakpoint
+ALTER TABLE "user_companies" DROP COLUMN "company_picture_file_name";--> statement-breakpoint
+ALTER TABLE "users" DROP COLUMN "profile_picture_file_name";
