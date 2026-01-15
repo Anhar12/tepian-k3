50efceb68d8125001be14c53ba7c8386481d039d
rizrmdhn <114452320+rizrmdhn@users.noreply.github.com>
2026-01-15 14:04:45 +0800

    feat: update tool selection labels in create and edit routes

--- apps/web/src/routes/(core)/back-office/tools/create.tsx
+++ apps/web/src/routes/(core)/back-office/tools/create.tsx
@@ -28,7 +28,12 @@ import { trpc } from "@/utils/trpc";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useMutation } from "@tanstack/react-query";
 import { createFileRoute } from "@tanstack/react-router";
-import { TOOLS_AVAILABILITY, TOOLS_CONDITIONS } from "@tepian-k3/constants";
+import {
+  TOOLS_AVAILABILITY,
+  TOOLS_AVAILABILITY_LABELS,
+  TOOLS_CONDITIONS,
+  TOOLS_CONDITIONS_LABELS,
+} from "@tepian-k3/constants";
 import toolsSchema from "@tepian-k3/schema/tools.schema";
 import { LoaderCircle } from "lucide-react";
 import { Controller, useForm } from "react-hook-form";
@@ -417,7 +422,7 @@ function RouteComponent() {
                       <SelectContent>
                         {TOOLS_CONDITIONS.map((condition) => (
                           <SelectItem key={condition} value={condition}>
-                            {condition}
+                            {TOOLS_CONDITIONS_LABELS[condition]}
                           </SelectItem>
                         ))}
                       </SelectContent>
@@ -454,7 +459,7 @@ function RouteComponent() {
                       <SelectContent>
                         {TOOLS_AVAILABILITY.map((availability) => (
                           <SelectItem key={availability} value={availability}>
-                            {availability}
+                            {TOOLS_AVAILABILITY_LABELS[availability]}
                           </SelectItem>
                         ))}
                       </SelectContent>
