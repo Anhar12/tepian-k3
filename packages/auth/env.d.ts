fa4d809eb7038a8f9cf2bb96adce2e4dcf13f42c
rizrmdhn <114452320+rizrmdhn@users.noreply.github.com>
2026-01-25 20:38:37 +0800

    chore(auth, db, services): remove unused environment variable files and related code

--- packages/auth/env.d.ts
+++
@@ -1,9 +0,0 @@
-export declare const env: Readonly<{
-    JWT_SECRET: string;
-    JWT_REFRESH_SECRET: string;
-    JWT_RESET_PASSWORD_SECRET: string;
-    JWT_ACCESS_TOKEN_EXPIRY: string;
-    JWT_REFRESH_TOKEN_EXPIRY: string;
-    APP_URL: string;
-}>;
-//# sourceMappingURL=env.d.ts.map
--- packages/db/env.d.ts
+++
@@ -1,4 +0,0 @@
-export declare const env: Readonly<{
-    POSTGRES_URL: string;
-}>;
-//# sourceMappingURL=env.d.ts.map
