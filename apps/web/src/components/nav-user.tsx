7d39b842bb5caa909c11adda42195515d8e44c4a
Noor Rizki Ramadhan <rizrmdhn.work@gmail.com>
2026-02-06 10:19:37 +0800

    feat: remove profile menu item from NavUser component

--- apps/web/src/components/nav-user.tsx
+++ apps/web/src/components/nav-user.tsx
@@ -3,7 +3,6 @@ import {
   IconHome,
   IconLogout,
   IconSettings,
-  IconUserCircle,
 } from "@tabler/icons-react";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import {
@@ -102,10 +101,6 @@ export function NavUser() {
               <DropdownMenuItem onSelect={() => navigate({ to: "/" })}>
                 <IconHome />
                 Beranda
-              </DropdownMenuItem>
-              <DropdownMenuItem onSelect={() => navigate({ to: "/profile" })}>
-                <IconUserCircle />
-                Profil Saya
               </DropdownMenuItem>
               <DropdownMenuItem>
                 <ModeToggle />
