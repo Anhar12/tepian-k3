95bb6c6a037ca811fb08079cf938ff4bd3189f36
Noor Rizki Ramadhan <rizrmdhn.work@gmail.com>
2026-05-29 20:02:51 +0800

    feat(migrations): add new journal entry for mushy maddog version 7
    - added entry with index 5 for version 7
    - included timestamp and tag for new migration

---
+++ packages/db/src/migrations/0005_mushy_maddog.sql
@@ -0,0 +1,9 @@
+ALTER TABLE "document_signatures" DROP CONSTRAINT "document_signatures_signed_by_user_id_users_id_fk";
+--> statement-breakpoint
+ALTER TABLE "documents" DROP CONSTRAINT "documents_uploaded_by_user_id_users_id_fk";
+--> statement-breakpoint
+ALTER TABLE "documents" DROP CONSTRAINT "documents_signed_by_user_id_users_id_fk";
+--> statement-breakpoint
+ALTER TABLE "document_signatures" ADD CONSTRAINT "document_signatures_signed_by_user_id_users_id_fk" FOREIGN KEY ("signed_by_user_id") REFERENCES "public"."users"("id") ON DELETE set null ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "documents" ADD CONSTRAINT "documents_uploaded_by_user_id_users_id_fk" FOREIGN KEY ("uploaded_by_user_id") REFERENCES "public"."users"("id") ON DELETE set null ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "documents" ADD CONSTRAINT "documents_signed_by_user_id_users_id_fk" FOREIGN KEY ("signed_by_user_id") REFERENCES "public"."users"("id") ON DELETE set null ON UPDATE no action;
